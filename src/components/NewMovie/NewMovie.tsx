92b8a9ae8ccfbef23b5ebf8b2de5eaf2a79ae544
Daria Achkasova <daria.achkasova6@gmail.com>
2026-02-17 15:12:25 +0100

    Solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,10 +1,64 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { TextField } from '../TextField';
 
-export const NewMovie = () => {
+type Movie = {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+};
+
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, cetCount] = useState(0);
+  const [valueTitle, setValueTitle] = useState('');
+  const [valueDescription, setValueDescription] = useState('');
+  const [valueImage, setValueImage] = useState('');
+  const [valueImdb, setValueImdb] = useState('');
+  const [valueID, setValueID] = useState('');
+
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    const requiredFields = [valueTitle, valueImage, valueImdb, valueID];
+    const hasEmptyField = requiredFields.some(field => !field.trim());
+
+    setHasError(hasEmptyField);
+  }, [valueTitle, valueImage, valueImdb, valueID]);
+
+  function reset() {
+    setValueTitle('');
+    setValueDescription('');
+    setValueImage('');
+    setValueImdb('');
+    setValueID('');
+    cetCount(prev => prev + 1);
+  }
+
+  const hadleSabmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (hasError) {
+      return;
+    }
+
+    const newMovie: Movie = {
+      title: valueTitle.trim(),
+      description: valueDescription.trim(),
+      imgUrl: valueImage.trim(),
+      imdbUrl: valueImdb.trim(),
+      imdbId: valueID.trim(),
+    };
+
+    onAdd(newMovie);
+    reset();
+  };
 
   return (
     <form className="NewMovie" key={count}>
@@ -13,18 +67,41 @@ export const NewMovie = () => {
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={valueTitle}
+        onChange={setValueTitle}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={valueDescription}
+        onChange={setValueDescription}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={valueImage}
+        onChange={setValueImage}
+        required
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={valueImdb}
+        onChange={setValueImdb}
+        required
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={valueID}
+        onChange={setValueID}
+        required
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +109,8 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={hasError}
+            onClick={hadleSabmit}
           >
             Add
           </button>
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -20,14 +20,21 @@ export const TextField: React.FC<Props> = ({
   label = name,
   placeholder = `Enter ${label}`,
   required = false,
-  onChange = () => {},
+  onChange,
 }) => {
   // generate a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
 
   // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
+
   const hasError = touched && required && !value;
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (onChange) {
+      onChange(event.target.value);
+    }
+  };
 
   return (
     <div className="field">
@@ -45,7 +52,7 @@ export const TextField: React.FC<Props> = ({
           })}
           placeholder={placeholder}
           value={value}
-          onChange={event => onChange(event.target.value)}
+          onChange={handleChange}
           onBlur={() => setTouched(true)}
         />
       </div>
